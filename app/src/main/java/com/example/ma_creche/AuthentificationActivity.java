af7e5f857c56f9371b8eac68901b013eaae3b9fc
lamine1305 <lamine1305@gmail.com>
2020-04-30 15:42:38 +0200

    ajout de la partie authentification

---
+++ app/src/main/java/com/example/ma_creche/AuthentificationActivity.java
@@ -0,0 +1,15 @@
+package com.example.ma_creche;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentification);
+        System.out.println("je suis dans la partie authentification");
+    }
+}
--- app/src/main/java/com/example/ma_creche/MainActivity.java
+++ app/src/main/java/com/example/ma_creche/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.ma_creche;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button butonConnect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.butonConnect= findViewById(R.id.butonConnect);
+
+        this.butonConnect.setOnClickListener((View v)->
+                {
+               Intent inte = new Intent(this,AuthentificationActivity.class);
+              // inte.putExtra("nom","adam");
+                    startActivity(inte);
+                System.out.println("salut ....");
+                }
+
+                );
+
     }
 }
