835e7003b911c2e198f3282729c0b77c7e7060ea
alili <fr.alili84@gmail.com>
2020-04-30 17:40:09 +0200

    Ajout de lien entre la page d'accueil et la page d'authentification partie parent

--- app/src/main/java/com/example/ma_creche/MainActivity.java
+++ app/src/main/java/com/example/ma_creche/MainActivity.java
@@ -11,13 +11,21 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button butonEnseignant;
+    Button butonParent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
                this.butonEnseignant= findViewById(R.id.butonEnseignant);
+               this.butonParent=findViewById(R.id.buttonParent);
 
+               this.butonParent.setOnClickListener((View v)->{
+                 Intent intent= new Intent(this, AuthentificationActivity.class);
+                 intent.putExtra("categorie","parent");
+                   startActivity(intent);
+                   System.out.println("salut Parent...");
+        });
              this.butonEnseignant.setOnClickListener((View v)->
                 {
                Intent intent = new Intent(this,AuthentificationActivity.class);
