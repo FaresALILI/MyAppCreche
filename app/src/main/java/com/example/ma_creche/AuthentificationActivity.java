92ed0763d038ac64b774d0548c5873ba71583497
lamine1305 <lamine1305@gmail.com>
2020-05-02 16:59:42 +0200

    gestion statique des notification via l'ecran principal des activités

--- app/src/main/java/com/example/ma_creche/AuthentificationActivity.java
+++ app/src/main/java/com/example/ma_creche/AuthentificationActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class AuthentificationActivity extends AppCompatActivity {
-
+Button btnAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
         System.out.println("je suis dans la partie authentification");
+        btnAuth= findViewById(R.id.butonConnect);
+        btnAuth.setOnClickListener(v->{
+            Intent intent= new Intent(this, BlocActivity.class);
+            startActivity(intent);
+        });
     }
 }
