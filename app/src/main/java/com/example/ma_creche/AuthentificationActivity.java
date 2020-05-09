a6155446e13239658e615f6ae97f59e106ec5846
lamine1305 <lamine1305@gmail.com>
2020-05-09 15:50:51 +0200

    partie design de l'authentification

--- app/src/main/java/com/example/ma_creche/AuthentificationActivity.java
+++ app/src/main/java/com/example/ma_creche/AuthentificationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ma_creche.utils.CategirieUser;
@@ -15,12 +16,14 @@ public class AuthentificationActivity extends AppCompatActivity {
 Button btnAuth;
 EditText editlogin;
 EditText editmotpass;
+TextView textCreateAccount;
 CategirieUser cat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
-        btnAuth= findViewById(R.id.butonConnect);
+        btnAuth = findViewById(R.id.butonConnect);
+        textCreateAccount = findViewById(R.id.textViewCreateAccount);
         btnAuth.setOnClickListener(v->{
             Intent intent= new Intent(this, BlocActivity.class);
             editlogin=findViewById(R.id.editLogin);
@@ -32,6 +35,14 @@ CategirieUser cat;
             startActivity(intent);
             else
                 Toast.makeText(getApplicationContext(), "Veuillez-vous authentifier", Toast.LENGTH_LONG).show();
+        });
+
+        //Aller à l'écran de création d'un compte
+
+        textCreateAccount.setOnClickListener(v->{
+            Intent intent= new Intent(this, CreationCompteActivity.class);
+                startActivity(intent);
+
         });
     }
 }
