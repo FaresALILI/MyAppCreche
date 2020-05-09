742ecf6a451ee36d669628f9f56b5539d419cb10
lamine1305 <lamine1305@gmail.com>
2020-05-09 19:26:14 +0200

    enregitrement en bdd

--- app/src/main/java/com/example/ma_creche/CreationCompteActivity.java
+++ app/src/main/java/com/example/ma_creche/CreationCompteActivity.java
@@ -1,16 +1,22 @@
 package com.example.ma_creche;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreationCompteActivity extends AppCompatActivity {
 
@@ -18,7 +24,11 @@ public class CreationCompteActivity extends AppCompatActivity {
     EditText editlogin;
     EditText editPassword;
     EditText editPasswordConfirm;
+    String login;
+    String pass;
     FirebaseAuth fireAuth;
+    FirebaseFirestore firestore;
+    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +41,16 @@ public class CreationCompteActivity extends AppCompatActivity {
         editPasswordConfirm=findViewById(R.id.editPassConfirm);
 
         this.btnRegister.setOnClickListener(v->{
-            String login=editlogin.getText().toString();
-            String pass=editPassword.getText().toString();
+             login=editlogin.getText().toString();
+             pass=editPassword.getText().toString();
             String passConfirm=editPasswordConfirm.getText().toString();
 
             if(TextUtils.isEmpty(login)) {
                 this.editlogin.setError("le login doit etre renseigné");
+                return;
+            }
+            else if(!login.contains("@")||(!login.contains("."))) {
+                this.editlogin.setError("Veuillez saisir une adresse mail correcte");
                 return;
             }
             else if(pass.length()<6){
@@ -51,7 +65,20 @@ public class CreationCompteActivity extends AppCompatActivity {
             fireAuth.createUserWithEmailAndPassword(login,pass).addOnCompleteListener(task->{
                     if(task.isSuccessful()){
                     Toast.makeText(this,"compte créé",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(getApplicationContext(),BlocActivity.class));
+                    this.userID = fireAuth.getCurrentUser().getUid();
+
+                    //Enregistrement en base de données
+                    DocumentReference documentRef =firestore.collection("users").document(userID);
+                    Map<String,Object> user= new HashMap<>();
+                    user.put("email",login);
+                    user.put("password",pass);
+                    user.put("date_creation",new Date());
+                    documentRef.set(user).addOnSuccessListener(x->{
+
+                       Log.d("TAG","utilisateur a été enregistré en base"+ userID);
+                        startActivity(new Intent(getApplicationContext(),BlocActivity.class));
+
+                    });
                     }
                     else{
                         Toast.makeText(this,"Erreur lors de la création de votre compte"+task.getException(),Toast.LENGTH_SHORT).show();
