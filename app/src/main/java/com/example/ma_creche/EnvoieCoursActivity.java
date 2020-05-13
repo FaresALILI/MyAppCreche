8275d0b539917fd0d82335a43240a1029416f838
alili <fr.alili84@gmail.com>
2020-05-13 19:41:58 +0200

    select and upload file

--- app/src/main/java/com/example/ma_creche/EnvoieCoursActivity.java
+++ app/src/main/java/com/example/ma_creche/EnvoieCoursActivity.java
@@ -13,14 +13,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.ma_creche.utils.CategirieUser;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EnvoieCoursActivity extends AppCompatActivity {
 
@@ -37,10 +42,14 @@ public class EnvoieCoursActivity extends AppCompatActivity {
     FirebaseDatabase database;
     ProgressDialog progressDialog;
     InputStream image;
+    private static final int PICK_FILE=1;
+    ArrayList<Uri> FilList=new ArrayList<Uri>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_envoie_cours);
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setMessage("Processing Please Wait.....");
 
         this.editTextDesc = findViewById(R.id.editTextDescription);
         this. btnDecon= findViewById(R.id.buttonDeconnexion);
@@ -51,15 +60,27 @@ public class EnvoieCoursActivity extends AppCompatActivity {
         storage= FirebaseStorage.getInstance();//return une instance de firebase storage
         database=FirebaseDatabase.getInstance();//return une instance de firebase Database
 
+        this.btnSelectFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFile(v);
 
-
+            }
+        });
+        this.btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile(v);
+            }
+        });
+/*
         this.btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 download();
             }
         });
-
+*/
         this.txtVwNotification =  findViewById(R.id.textViewNotification);
         this.btnEnvoiCours.setOnClickListener(v->{
             if(this.editTextDesc.getText().toString().isEmpty())
@@ -71,7 +92,6 @@ public class EnvoieCoursActivity extends AppCompatActivity {
             }
         });
 
-
         this.btnDecon.setOnClickListener((View v)->
                 {
                     this.cat.deconnexion();
@@ -80,7 +100,7 @@ public class EnvoieCoursActivity extends AppCompatActivity {
                 }
         );
     }
-
+/*
     private void download() {
         StorageReference storageReference =storage.getInstance().getReference();
         reference = storageReference.child("davidson-lamine-mehidi.pdf");
@@ -89,7 +109,6 @@ public class EnvoieCoursActivity extends AppCompatActivity {
             downloadFile(EnvoieCoursActivity.this,"davidson-lamine-mehidi",".pdf", Environment.DIRECTORY_DOWNLOADS,url);
         }).addOnFailureListener(e->{
         });
-
     }
     private void downloadFile(Context context, String fileName, String extension, String destination, String url) {
         DownloadManager downloadManager = (DownloadManager)context.getSystemService(context.DOWNLOAD_SERVICE);
@@ -98,10 +117,59 @@ public class EnvoieCoursActivity extends AppCompatActivity {
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
         request.setDestinationInExternalFilesDir(context,destination,fileName+extension);
         downloadManager.enqueue(request);
+    }*/
+
+    public void selectFile(View view){
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        startActivityForResult(intent,PICK_FILE);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==PICK_FILE){
+            if(resultCode==RESULT_OK){
+                if (data.getClipData() != null){
+                    int count=data.getClipData().getItemCount();
+                    int i=0;
+                    while (i<count){
+                        Uri File=data.getClipData().getItemAt(i).getUri();
+                        FilList.add(File);
+                        i++;
+                    }
+                    Toast.makeText(this,"You have selected "+ FilList.size()+" Files", Toast.LENGTH_LONG).show();
 
+                }
+            }
+        }
+    }
 
+    public void uploadFile(View view){
+        progressDialog.show();
+        Toast.makeText(this,"if takes time , you can press Again",Toast.LENGTH_SHORT).show();
+        for (int j=0;j>FilList.size();j++){
+            Uri PerFile=FilList.get(j);
+            StorageReference folder=FirebaseStorage.getInstance().getReference().child("ResMyAppCreche");
+            StorageReference filename=folder.child("file"+PerFile.getLastPathSegment());
+            filename.putFile(PerFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    filename.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            DatabaseReference databaseReference=FirebaseDatabase.getInstance().getReference().child("user");
+                            HashMap<String,String> hashMap=new HashMap<>();
+                            hashMap.put("link",String.valueOf(uri));
+                            databaseReference.push().setValue(hashMap);
+                            progressDialog.dismiss();
+                            FilList.clear();
+                        }
+                    });
+                }
+            });
+        }
 
-
+    }
 }
